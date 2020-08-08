cbac27170640c1a2f8be279326d8ca5f5e2cd371
Mike1000e <denis.luckash@gmail.com>
2020-08-07 18:31:05 -0700

    wowSOcool

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,5 +1,86 @@
 package _05_typing_tutor;
 
-public class TypingTutor {
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
 
-}
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TypingTutor implements KeyListener {
+	
+	static JFrame frame = new JFrame();
+	static JLabel label = new JLabel();
+	static JPanel panel = new JPanel();
+	
+	static char currentLetter;
+	
+	public static void main(String[] args) {
+	currentLetter = generateRandomLetter();
+	
+	TypingTutor type = new TypingTutor();
+	type.randomMethod();
+	frame.setVisible(true);
+	frame.setTitle("TYPE THE LETTER NOW");
+
+	label.setFont(label.getFont().deriveFont(28.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	
+	
+	label.setText(currentLetter+"");
+	panel.add(label);
+	frame.add(panel);
+	frame.setSize(300,300);
+	frame.pack();
+	
+
+	}
+	 void randomMethod() {
+		frame.addKeyListener(this);
+	
+	}
+	
+	static char generateRandomLetter() {
+	    Random r = new Random();
+	    return (char) (r.nextInt(26) + 'a');
+	    
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char letterTyped = e.getKeyChar();
+		String letter = currentLetter+"";
+		if(letter.equalsIgnoreCase(letterTyped+"")) {
+			panel.setBackground(Color.GREEN);
+			
+			frame.pack();
+			
+		}else {
+			panel.setBackground(Color.RED);
+			frame.pack();
+			
+			
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		currentLetter = generateRandomLetter();
+		label.setText("");
+		label.setText ("        "+currentLetter+"        ");
+
+	
+		frame.pack();
+	}
+
+	}
+
